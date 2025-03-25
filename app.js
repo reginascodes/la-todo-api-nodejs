1192e0c34fc0c413b61cbb522163f4666f9a050d
Regina Abdao <rciara.abdao@gmail.com>
2025-03-25 22:38:14 +0800

    feat: added request validations

--- app.js
+++ app.js
@@ -15,20 +15,40 @@ app.listen(PORT, () => {
     console.log('Server is running on port:', PORT);
 });
 
+function validateTask(req, res, next) {
+    const {title, description, status, order} = req.body
+
+    if (!title) {
+        return res.status(400).json({
+            message: "Title is required."
+        });
+    }
+
+    const validStatuses = ['Done', 'Pending'];
+    if (!validStatuses.includes(status)) {
+        return res.status(400).json({
+            message: 'Status must be either "Done" or "Pending".'
+        });
+    }
+
+    next();
+      
+}
+
 // GET tasks
 app.get('/tasks', (req, res) => {
     res.json(tasks);
 });
 
 // CREATE task
-app.post('/tasks', (req, res) => {
+app.post('/tasks', validateTask, (req, res) => {
     const task = req.body;
     tasks.push(task);
     res.json(task);
 });
 
 // UPDATE task
-app.put('/tasks/:id', (req, res) => {
+app.put('/tasks/:id', validateTask, (req, res) => {
     const id = req.params.id;
     const task = req.body;
     const taskIndex = tasks.findIndex(task => task.id == id);
@@ -51,6 +71,9 @@ app.post('/tasks/move', (req, res) => {
 
     // Find the task and get its index
     const taskIndex = tasks.findIndex(task => task.id == id);
+    if (taskIndex === -1) {
+        return res.status(404).json({ message: 'Task not found.' });
+    }
 
     // Remove the task from the array
     // Returns array with the removed task
