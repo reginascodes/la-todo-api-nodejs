4a640612cf1151492ad47e4ebf4f37c1f767a084
Regina Abdao <rciara.abdao@gmail.com>
2025-03-25 21:05:49 +0800

    chore: initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Server is running on port:', PORT);
+});
+
+app.get('/status', (req, res) => {
+    const status = {
+        status: 'Running'
+    };
+    
+    res.json(status);
+});
